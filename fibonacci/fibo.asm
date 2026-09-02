e9bc3eea5902e8deee2728fb7546b283aca7763f
Beg846 <hoangminhthai5995@gmail.com>
2026-09-02 16:17:08 +0700

    add fibo

---
+++ fibonacci/fibo.asm
@@ -0,0 +1,104 @@
+section .bss
+    input_fibo resb 20
+    output_fibo resb 20
+
+section .data
+    input_msg db 'Input:', 10
+    input_msg_len equ $ - input_msg
+
+section .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, input_msg
+    mov rdx, input_msg_len
+    syscall
+
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, input_fibo
+    mov rdx, 20
+    syscall
+
+    call str_to_int
+    mov r15, rax
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+
+fibo:
+    ;r12 = 0 r13 = 1
+    ;r12 = 1 r13 = 1
+    ;r12 = 2 r13 = 1
+    ;r8 = r12
+    ;r12 = r12 + r13
+    ;r13 = r8
+    ;r8 = r12
+    mov r12, 0
+    mov r13, 1
+
+.fibo_loop:
+    dec r15
+    mov rax, r12
+    xchg r12, r13
+    add r12, r13
+    call int_to_str
+    jmp .fibo_print
+    
+.fibo_print:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, output_fibo
+    mov rdx, rcx
+    cmp r15, 0
+    jg fibo_loop
+
+.fibo_done:
+    ret
+
+
+
+int_to_str:
+    xor rcx, rcx
+    xor rdx, rdx
+    mov rbx, 10
+    mov rsi, output_fibo
+    add rsi, 19
+
+.convert
+    xor rdx, rdx
+    div rbx
+    add rdx, 48
+    dec rsi
+    mov [rsi], dl
+    inc rcx
+    cmp rax, 0
+    jg .convert
+
+    ret
+
+
+
+str_to_int:
+    xor rax, rax ;so sánh 2 thanh ghi theo xor nếu bằng thì trả về 0, nếu không bằng trả về 1
+    xor rbx, rbx
+    mov bl, [rsi]
+    cmp bl, 10
+    je .num_read_done
+
+.num_read:
+    ;n = n*10 + bl - 48('0')    
+    imul rax, rax, 10
+    sub bl, 48
+    add rax, rbx
+    inc rsi
+    mov bl, [rsi]
+    cmp bl, 10
+    jne .num_read
+
+.num_read_done:
+    ret
